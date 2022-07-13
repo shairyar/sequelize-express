e1f604abf1d8f6fc65bca33174cea89f5c90904d
Shairyar Baig <shairyarbaig@gmail.com>
2022-07-13 15:48:26 +0500

    mysql integrated and working

--- express-main-example/sequelize/index.js
+++ express-main-example/sequelize/index.js
@@ -4,11 +4,18 @@ const { applyExtraSetup } = require('./extra-setup');
 // In a real app, you should keep the database connection URL as an environment variable.
 // But for this example, we will just use a local SQLite database.
 // const sequelize = new Sequelize(process.env.DB_CONNECTION_URL);
-const sequelize = new Sequelize({
-	dialect: 'sqlite',
-	storage: 'sqlite-example-database/example-db.sqlite',
-	logQueryParameters: true,
-	benchmark: true
+
+const sequelize = new Sequelize('sequelize', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
 });
 
 const modelDefiners = [
